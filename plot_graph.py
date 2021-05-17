c8c46c070d5cbe2d4f2936ae9d7fb89c846b49f1
Christo George <chrizgeo@gmail.com>
2021-05-17 23:42:37 +0200

    Updated plot_graph functions

--- plot_graph.py
+++ plot_graph.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from pandas import read_csv
 import csv
+import math
 
 """ return a pandas dataframe """
 def load_data(filename):
@@ -9,11 +10,39 @@ def load_data(filename):
             return read_csv(filename, sep=',', keep_default_na=False, na_values=[''])
     except FileNotFoundError as e:
         raise e
-
-dataFrame = load_data("statistics.csv")
-print(dataFrame)
-print(dataFrame.dtypes)
-dataFrame.plot(x='channelnum',y='utilisation',kind='line')
-dataFrame.plot(x='channelnum',y='deadlineMissRatio',kind='line')
+    
+def split_dataframe(df, chunk_size=25):
+    chunks = list()
+    num_chunks = math.ceil(len(df)/chunk_size)
+    for i in range(num_chunks):
+        chunks.append(df[i*chunk_size:(i+1)*chunk_size])
+    return chunks, num_chunks
+# Get the dataframe from the csv file.
+df = load_data("statistics.csv")
+df[['period', 'deadline']] = df[['period', 'deadline']].astype(str)
+df_list, num_chunks = split_dataframe(df,25)
+ax = plt.gca()
+for i in range(num_chunks):
+    label_string = df_list[i]['Qmethod'].any()
+    if(label_string == "FCFS"):
+        label_string = "EDF" + " P:" + df_list[i]['period'].any() + " D:" + df_list[i]['deadline'].any()
+        df_list[i].plot(x='channelnum',y='utilisation',kind='line',label=label_string,ax=ax)
+        ax.set_xticks(df_list[i].channelnum)
+    elif(label_string == "PRIO"):
+        label_string = label_string + " P:" + df_list[i]['period'].any() + " D:" + df_list[i]['deadline'].any()
+        df_list[i].plot(x='channelnum',y='utilisation',kind='line',label=label_string,ax=ax,linestyle='--')
+        ax.set_xticks(df_list[i].channelnum)
 plt.show()
 
+ax = plt.gca()
+for i in range(num_chunks):
+    label_string = df_list[i]['Qmethod'].any()
+    if(label_string == "FCFS"):
+        label_string = "EDF" + " P:" + df_list[i]['period'].any() + " D:" + df_list[i]['deadline'].any()
+        df_list[i].plot(x='channelnum',y='deadlineMissRatio',kind='line',label=label_string,ax=ax)
+        ax.set_xticks(df_list[i].channelnum)
+    elif(label_string == "PRIO"):
+        label_string = label_string + " P:" + df_list[i]['period'].any() + " D:" + df_list[i]['deadline'].any()
+        df_list[i].plot(x='channelnum',y='deadlineMissRatio',kind='line',label=label_string,ax=ax,linestyle='--')
+        ax.set_xticks(df_list[i].channelnum)
+plt.show()
